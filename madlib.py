fcf02404680c6cddcf512febd6854cc0043835cf
Gul Fıdan Ozturk <102385902+gulfidan34@users.noreply.github.com>
2022-05-05 15:59:50 -0700

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,22 @@
+#prompt a user to enter a name:
+name = input('What is your name?: ')
+#prompt a user for an adjective
+adjective = input("enter an adjective: ")
+#prompt a user for a Plural noun:
+noun = input("enter a plural noun: ")
+#prompt a user for a noun, either a person or a thing:
+noun2 = input("enter any noun:")
+
+
+#print Ryhme
+print("Roses are"+ " "+ adjective)
+print(noun+ " " + "is blue")
+print("I can not start my day without"+ ' ' + noun2)
+
+
+
+
+output:
+Roses are cute
+moon is blue
+I can not start my day without coffee
